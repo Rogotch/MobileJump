0f13c3da3fa2aa57644d6a66969999a2c7e4607d
Rogotch <rogotch@gmail.com>
2022-11-02 17:17:06 +0300

    Tools Update

--- Assets/Resources/Scripts/Character.cs
+++ Assets/Resources/Scripts/Character.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
+    enum CharacterCondition { idle, move, fall }
 
     protected Rigidbody2D Character_rb;
     protected Vector2 speed_force = Vector2.zero;
+    protected Vector2Int grid_position = Vector2Int.zero;
 
+    CharacterCondition Condition;
 
     void Start()
     {
         Character_rb = gameObject.GetComponent<Rigidbody2D>();
+        Condition = CharacterCondition.idle;
+
     }
 }
--- Assets/Resources/Scripts/Player.cs
+++ Assets/Resources/Scripts/Player.cs
@@ -1,9 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
+using DG.Tweening;
 
 public class Player : Character
 {
+
     float horizontalInput;
     float jumpInput;
 
@@ -14,6 +18,11 @@ public class Player : Character
     public float jump_mod = 1f;
 
     public bool on_ground_flag = true;
+    public GameObject CellDetector;
+
+    public GridLayout gl;
+    public Tilemap tl;
+    public Tile tile;
 
     void FixedUpdate()
     {
@@ -25,7 +34,7 @@ public class Player : Character
         horizontalInput = Input.GetAxisRaw("Horizontal");
         if (Input.GetAxisRaw("Jump") != 0 && on_ground_flag)
         {
-            //Debug.Log("Jump");
+            Debug.Log(gl.WorldToCell(gameObject.transform.position));
             on_ground_flag = false;
             jumpInput = Input.GetAxisRaw("Jump") * jump_mod;
         }
@@ -34,6 +43,20 @@ public class Player : Character
             jumpInput = 0;
         }
 
+        if (horizontalInput != 0f)
+        {
+            var cellPosition = gl.WorldToCell(transform.position) + Vector3Int.down;
+            grid_position = new Vector2Int(cellPosition.x, cellPosition.y);
+            //cellPosition.z = 1;
+            Debug.Log(tl.GetTile(cellPosition));
+
+            Vector3 cell_pos = gl.CellToWorld(cellPosition);
+            tl.SetTile(cellPosition, tile);
+            cell_pos.z = CellDetector.gameObject.transform.position.z;
+            CellDetector.gameObject.transform.position = cell_pos - gl.cellSize/2;
+            //int axisValue = horizontalInput > 0 ? 1 : -1;
+            //Debug.Log("Horizontal " + Convert.ToString(axisValue));
+        }
         speed_force = new Vector2(horizontalInput, jumpInput) * speed_mod;
 
         Character_rb.velocity = new Vector2(Mathf.Lerp(speed_force.x, 0f, 0.001f), Character_rb.velocity.y + (jumpInput != 0 ? speed_force.y : 0f));
